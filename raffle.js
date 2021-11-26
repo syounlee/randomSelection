a887303aab1ecfff5f503e4f230f51012feecd80
Seung Y. Lee <seung.youn.lee@gmail.com>
2021-11-26 10:18:15 +0900

    commit on 'master'

--- raffle.js
+++ raffle.js
@@ -1,37 +1,56 @@
-const dotenv = require('dotenv').config();
+import fetch from 'node-fetch';
 
-const weightedSelection = require('./weighted-selection');
-const MersenneTwister = require('./mersenne-twister');
+import raffleWinners from 'raffleWinners';
+import Web3 from 'web3';
 
-const Web3 = require('web3');
-const axios = require('axios');
+import dotenv from 'dotenv'
+dotenv.config()
 
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.WEB3_INFURA_PROJECT_ID}`))
 
-const raffle = (contractAddress) => {
+async function raffle (contractAddress) {
   contractAddress = web3.utils.toChecksumAddress(contractAddress);
-  axios.get(`https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}`)
-  .then(response => {
-    abi = JSON.parse(response.data.result);
-    //console.log(abi);
-    const contract = new web3.eth.Contract(abi,contractAddress);
-    contract.methods.owner().call().then(console.log)
 
+  //////
+  // Retrieve the ABI of the contract given.
+  const etherscan_url = `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}`;
+  const res = await fetch(etherscan_url);
+  const abi = await res.json();
 
-    // const owner = new Promidr  contract.methods.owner().call());
-  })
-  .catch(error => {
-    console.log(error);
-    throw new Error();
+  ////////
+  // Connect to the Contract given the address and the ABI
+  const raffleContract = new web3.eth.Contract(abi,contractAddress);
+  
+  ////////
+  // Call the Contract to Retrive the information for the Raffle.
+  //////
+  /// 1. Base Price
+  const basePrice = raffleContract.methods.basePrice().call();
+  /// 2. Number of Winners
+  const numberOfWinners = raffleContract.methods.basePrice().call();
+  /// 3. Seed for the Random Number Generator
+  const seed = raffleContract.methods.basePrice().call();
+  /// 4. Participants Account Address
+  const participants = raffleContract.methods.participants().call();
+  /// 5. Participants Stake [arra]
+  const stakes = raffleContract.methods.stakes(participants).call();
+  /// 6. Participants token [array]
+  const tokens = raffleContract.methods.tokens(participants).call();
 
-  });
+  ///////
+  // Calculate Price Premium Rate for each participants
+  const pricePremiumRate = stakes.map(x => (x-basePrice)/basePrice);
 
-}
-
-
-
-
-
-
+  ///////
+  // Call the function 'raffleWinners' to get the indices of the winners in the array 'participants'.
+  winnersIndices = raffleWinners(seed, pricePremiumRate, tokens, numberOfWinners);
+  // Return the addresses of the winners
+  return winnersIndices.mpa(x => participants[x]);
+};
 
 module.exports = raffle;
+
+
+///// an example to call the function 'raffle'
+// const contractAddress = '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85';
+// raffle(contractAddress);
